3d1afd1cd55b4090462822c0639a7c0d0b8dd02f
Fabricio Curvello <tiofab@gmail.com>
2018-12-15 18:14:37 -0200

    Activities Despesa e Receita criadas e com temas ocultando ActionBar e Title

--- app/src/main/java/com/example/fabricio/drdespesa/activity/MainActivity.java
+++ app/src/main/java/com/example/fabricio/drdespesa/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.fabricio.drdespesa.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -33,12 +34,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void adicionarDespesa(View view) {
-
+        startActivity(new Intent(MainActivity.this, DespesasActivity.class));
     }
 
-
     public void adicionarReceita(View view) {
-
+        startActivity(new Intent(MainActivity.this, ReceitasActivity.class));
     }
 
     @Override
---
+++ app/src/main/java/com/example/fabricio/drdespesa/activity/ReceitasActivity.java
@@ -0,0 +1,15 @@
+package com.example.fabricio.drdespesa.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.fabricio.drdespesa.R;
+
+public class ReceitasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receitas);
+    }
+}
